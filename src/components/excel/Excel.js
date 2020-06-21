c04b90c8f674a9eaa9f286923e8a98e05c36dadb
Ivan Alenin <aiscorp@yandex.ru>
2020-06-21 04:10:15 +0300

    Components: table layout

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -6,37 +6,27 @@ export class Excel {
     this.components = options.components || []
   }
   getRoot() {
-    // const $root = document.createElement('div')
-    // $root.classList.add('excel')
     // $ from dom.js helps to make it shorter
     const $root = $.create('div', 'excel')
 
     this.components = this.components.map(Component => {
-      // const $el = document.createElement('div')
-      // $el.classList.add(Component.className)
       const $el = $.create('div', Component.className)
-
       const component = new Component($el)
-
-      // $el.innerHTML = component.toHTML()
       $el.html(component.toHTML())
       $root.append($el)
-      // $root.insertAdjacentHTML('beforeend', component.toHTML())
-      // console.log(component.toHTML())
       return component
     })
-    // console.log(this.components)
+
     return $root
   }
 
 
   render() {
-    // Node method insertAdjacentHTML()
+    // alternative method insertAdjacentHTML()
     // afterbegin, afterend, beforeend, beforebegin
     // this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`)
 
     this.$el.append(this.getRoot())
-
     this.components.forEach(component => component.init())
   }
 }
--- src/components/tabel/Table.js
+++ src/components/tabel/Table.js
@@ -1,77 +1,10 @@
 import {ExcelComponent} from '@core/ExcelComponent'
+import {createTable} from '@/components/tabel/table.template'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
   toHTML() {
-    return `
-      <div class="row">
-        <div class="row-info">
-
-        </div>
-        <div class="row-data">
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            D
-          </div>
-          <div class="column">
-            E
-          </div>
-          <div class="column">
-            F
-          </div>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="row-info">
-          1
-        </div>
-        <div class="row-data">
-          <div class="cel" contenteditable>cell</div>
-          <div class="cel" contenteditable>cell</div>
-          <div class="cel" contenteditable>cell</div>
-          <div class="cel" contenteditable>cell</div>
-          <div class="cel" contenteditable>cell</div>
-          <div class="cel" contenteditable>cell</div>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="row-info">
-          2
-        </div>
-        <div class="row-data">
-          <div class="cel" contenteditable>cell</div>
-          <div class="cel selected" contenteditable>cell</div>
-          <div class="cel" contenteditable>cell</div>
-          <div class="cel" contenteditable>cell</div>
-          <div class="cel" contenteditable>cell</div>
-          <div class="cel" contenteditable>cell</div>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="row-info">
-          3
-        </div>
-        <div class="row-data">
-          <div class="cel" contenteditable>cell</div>
-          <div class="cel" contenteditable>cell</div>
-          <div class="cel" contenteditable>cell</div>
-          <div class="cel" contenteditable>cell</div>
-          <div class="cel" contenteditable>cell</div>
-          <div class="cel" contenteditable>cell</div>
-        </div>
-      </div>
-    `
+    return createTable()
   }
 }
